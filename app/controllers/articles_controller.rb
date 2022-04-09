f6f4bcce8393c8a5ec56909466342a95598573d5
saberzero1 <ewjbangma@hotmail.com>
2022-04-09 04:08:18 +0200

    Updated new article view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,24 @@
 class ArticlesController < ApplicationController
+
     def new
         @article = Article.new
     end
+
+    def create
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        if @aticle.save
+            flash[:notice] = "Article was successfully created."
+            redirect_to articles_show(@article)
+        else
+            render 'new'
+        end
+    end
+
+    private
+        def article_params
+
+        params.require(:article).permit(:title, :description)
+        end
+
 end
