62c0d7d00037343c5c1c0e43e95943ee662c1731
saberzero1 <ewjbangma@hotmail.com>
2022-04-09 03:51:00 +0200

    Added database entry validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
+    validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 15000 }
 
 end
