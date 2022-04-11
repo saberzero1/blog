825aa2cb7bce9cef9887b041ec9624c2ba76bfb2
saberzero1 <ewjbangma@hotmail.com>
2022-04-11 11:56:28 +0200

    Added passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    has_secure_password
     has_many :articles
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
